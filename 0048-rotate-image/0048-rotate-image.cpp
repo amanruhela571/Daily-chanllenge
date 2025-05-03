057c3e187452087e12967221260e5e2757e22433
AMAN RUHELA <104575669+amanruhela571@users.noreply.github.com>
2025-05-03 18:32:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (8.22%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    void reve(vector<vector<int>>& matrix, int i){
+        int n=matrix.size();
+        for(int j=0;j<n/2;j++){
+            swap(matrix[i][j],matrix[i][n-j-1]);
+        }
+    }
     void rotate(vector<vector<int>>& matrix) {
         int n =matrix.size();
         vector<vector<int>> v(n, vector < int > (n, 0));
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<n;j++){
-                v[j][n-i-1]=(matrix[i][j]);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        matrix=v;
+        for(int i=0;i<n;i++){
+            reve(matrix,i);
+        }
     }
 };
